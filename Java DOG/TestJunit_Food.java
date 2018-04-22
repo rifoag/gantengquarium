5d0804531028782268637ddb832ece202e64568a
rifoag <rifoagenadi@gmail.com>
2018-04-23 04:38:23 +0700

    JUnit Food dan Dokumen
    - JUnit food done
    - nyicil laporan

---
+++ Java DOG/TestJunit_Food.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit_Food {
+
+   int x_pos = 500;
+   Food f = new Food(500);
+   int speed = 34;
+
+   @Test
+   public void testAdd() {
+      // konstruktor
+      assertEquals(x_pos,f.getAbsis(),0.5);
+
+      // getter speed
+      assertEquals(speed,f.getSpeed());
+   }
+}
